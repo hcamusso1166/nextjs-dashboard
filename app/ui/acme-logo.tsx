3810a198018c4124e221d0d04ef79e8835f3485a
unknown <hcamusso@gmail.com>
2024-08-09 14:48:23 -0300

    commit de sicc con next

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,20 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { roboto } from './fonts';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${roboto.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+       <Image
+            src="/SICCLOGO.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+      <p className="text-[44px]">SICC</p>
     </div>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Roboto } from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],   
+})
